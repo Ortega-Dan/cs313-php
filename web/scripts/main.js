35b7131bcf5c2e160354a068d9090d8cd630c53d
Dan Ortega <ortega.dan2010@gmail.com>
2019-01-22 22:54:23 -0500

    Team activity #2. Just playing around with Bootstrap, Jquery and some JS

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,3 +1,15 @@
 function clicked() {
     alert("Clicked!");
 }
+
+function changeColor() {
+    // Jquery
+    $("#div1").css({"background-color": $("#newcolor").val()});
+
+    // Vanilla JS
+    // document.getElementById("div1").style.backgroundColor = document.getElementById("newcolor").value;
+}
+
+
+function fadeDiv3() {
+}
