911267085a3ec1c13591f375f785f3451dbbeee6
Dan Ortega <ortega.dan2010@gmail.com>
2019-01-25 21:49:00 -0500

    Getting minimun requirements for week 2 personal assigment

---
+++ web/assignments.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CS 313 Assignment Portal | Dan Ortega</title>
+    <meta name="description" content="Assignment portal page for Dan Ortega in CS 313 class at Brigham Young University Idaho Online">
+    <link rel="stylesheet" href="styles/assign.css">
+</head>
+
+<body>
+    <header>
+        <img src="images/face.jpg" width="200" alt="My Profile Picture">
+        <h1>Dan Anderson Ortega Salinas</h1>
+    </header>
+    <nav>
+        <!-- Here we will have a main menu in the future -->
+    </nav>
+    <main>
+        <h2>CS 313 - Assignment Portal</h2>
+        <ul>
+            <li>Lesson XX: <a href="#">Assignments comming soon!</a></li>
+        </ul>
+    </main>
+    <footer>
+        <p>&copy;2017 &#9816; Dan Anderson Ortega Salinas &#9816; Colombia &#9816; <a href="http://byui.edu/online/">BYU-Idaho
+                Online Learning</a></p>
+    </footer>
+
+</body>
+
+</html>
--- web/index.php
+++ web/index.php
@@ -1,5 +1,42 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-// header('Location: hello.html');
-include('hello.html');
-// phpinfo();
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dan on Heroku</title>
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Black+Ops+One" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Monoton" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Sedgwick+Ave+Display" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">-->
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    <link rel="shortcut icon" href="images/favicon.ico">
+
+</head>
+
+<body>
+    <main>
+        <h1>Dan Ortega</h1>
+
+        <h3>Hi there ..</h3>
+        <p>Here is just the most simple site possible.
+
+            <br>
+            Thank you for comming !
+        </p>
+
+        <p><img src="images/face.jpg" width="200" alt="My Picture"></p>
+        <br>
+
+    </main>
+
+    <footer>
+        <p>
+            <a href="assignments.html">Go to assignments page</a>
+        </p>
+    </footer>
+
+</body>
+
+</html>
