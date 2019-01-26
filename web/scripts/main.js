d91c9b2522d60f2c30d7dea9e870fcad583495ff
Dan Ortega <ortega.dan2010@gmail.com>
2019-01-25 22:35:55 -0500

    Adding some JavaScript, bootstrap and PHP features

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,15 +1,4 @@
 function clicked() {
-    alert("Clicked!");
-}
-
-function changeColor() {
-    // Jquery
-    $("#div1").css({"background-color": $("#newcolor").val()});
-
-    // Vanilla JS
-    // document.getElementById("div1").style.backgroundColor = document.getElementById("newcolor").value;
-}
-
-
-function fadeDiv3() {
+    alert("You will be redirected by using JavaScript to the assignments page.\n Thank you for comming !!");
+    window.location.replace('assignments.php');
 }
