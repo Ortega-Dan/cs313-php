fc488a2689745d2c4d1c72f08f32cde0b85356ec
Dan Ortega <ortega.dan2010@gmail.com>
2019-01-30 23:29:36 -0500

    Creating Shopping Cart assignment with modularization and adding shopping browse functionality

---
+++ web/assignments/assignmentsmodules/fromdttouncomplhead.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dan on Heroku</title>
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Black+Ops+One" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Monoton" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Sedgwick+Ave+Display" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">-->
+
+    <!-- Bootstrap main styles -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+        crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="../generalassignmentstyles/styles.css">
+    <link rel="shortcut icon" href="../../images/favicon.ico">
+
+    <!--This is intended to allow the ability to implement assignment specific styles-->
