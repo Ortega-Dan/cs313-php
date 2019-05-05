d97c8865f63862dffe042a71ed1544f7abe6c438
Dan Ortega <ortega.dan2010@gmail.com>
2019-05-04 23:26:38 -0500

    Just getting code in the right place

---
+++ web/assignments/week1/index.php
@@ -0,0 +1,7 @@
+<?php
+
+include('hello.html');
+
+// echo phpversion();
+// header('Location: hello.html');
+// phpinfo();
--- web/index.php
+++ web/index.php
@@ -1,7 +1,63 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-include('hello.html');
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dan on Heroku</title>
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Black+Ops+One" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Monoton" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Sedgwick+Ave+Display" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">-->
 
-// echo phpversion();
-// header('Location: hello.html');
-// phpinfo();
+    <!-- Bootstrap main styles -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+        crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    <link rel="shortcut icon" href="images/favicon.ico">
+
+</head>
+
+<body>
+    <main>
+        <h1>Dan Ortega</h1>
+
+        <h3>Hi there ..</h3>
+        <p>Here is just the most simple site possible.
+
+            <br>
+            Thank you for comming !
+            <br>
+            <br>
+            As a brief introduction: I just like so much programming.
+        </p>
+
+        <p><img src="images/face.jpg" width="200" alt="My Picture"></p>
+        <br>
+
+    </main>
+
+    <footer>
+
+        <?php echo "Your public ip address now is: ".$_SERVER['REMOTE_ADDR']; ?>
+        <br>
+        <br>
+        <button type="button" onclick="clicked()" class="btn btn-info">Go to assignments</button>
+        <br>
+    </footer>
+
+    <script src="scripts/main.js"></script>
+
+
+    <!-- Bootstrap Scripts -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
