1ebb590971d2b6e9419d1c366eca32e8a6ad93ef
Dan Ortega <ortega.dan2010@gmail.com>
2019-05-29 19:33:14 -0500

    Starting directories

---
+++ web/assignments/week5/dbaccess/dbconnect.php
@@ -0,0 +1,20 @@
+<?php
+
+function getMyConnection()
+{
+
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbopts = parse_url($dbUrl);
+
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    $dbName = ltrim($dbopts["path"], '/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    return $db;
+
+}
