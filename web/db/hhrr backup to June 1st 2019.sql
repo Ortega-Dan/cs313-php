b7d2644eaa69df30a260c122737f3183f1a7984d
Dan Ortega <ortega.dan2010@gmail.com>
2019-06-06 12:17:28 -0500

    Updating DB

--- web/db/hhrr backup to June 1st 2019.sql
+++ web/db/hhrr backup to June 1st 2019.sql
@@ -21,6 +21,7 @@ INSERT INTO public.employee (keyid, name, personalid, entrydate) VALUES (6, 'Jos
 INSERT INTO public.employee (keyid, name, personalid, entrydate) VALUES (7, 'Steed Parker', 6963654, '2008-12-23');
 INSERT INTO public.employee (keyid, name, personalid, entrydate) VALUES (8, 'Ashley Gonzalez', 1915615, '2015-09-12');
 
+
 create table client
 (
     keyid    serial      not null
@@ -43,6 +44,7 @@ INSERT INTO public.client (keyid, name, socialid) VALUES (3, 'Level', 784539);
 INSERT INTO public.client (keyid, name, socialid) VALUES (4, 'Alpina', 321458);
 INSERT INTO public.client (keyid, name, socialid) VALUES (5, 'Addidas', 91569);
 INSERT INTO public.client (keyid, name, socialid) VALUES (6, 'PriceSmart', 54918);
+
 
 create table address
 (
@@ -68,6 +70,14 @@ INSERT INTO public.address (keyid, client_id, employee_id, physical_address) VAL
 INSERT INTO public.address (keyid, client_id, employee_id, physical_address) VALUES (5, 0, 2, '5 Round Pkwy, 18915');
 INSERT INTO public.address (keyid, client_id, employee_id, physical_address) VALUES (6, 0, 3, '543 Fair Hw, 16815');
 INSERT INTO public.address (keyid, client_id, employee_id, physical_address) VALUES (7, 6, 0, '84 Super Dr, 86413');
+INSERT INTO public.address (keyid, client_id, employee_id, physical_address) VALUES (8, 5, 0, 'Addidas Address Somewhere');
+INSERT INTO public.address (keyid, client_id, employee_id, physical_address) VALUES (9, 0, 4, 'Nicest House at 1st Drive');
+INSERT INTO public.address (keyid, client_id, employee_id, physical_address) VALUES (10, 0, 8, 'Nice Apartment at 1st Drive SLC Utah');
+INSERT INTO public.address (keyid, client_id, employee_id, physical_address) VALUES (11, 1, 0, 'Calle 26 #34, Bog, Colombia');
+INSERT INTO public.address (keyid, client_id, employee_id, physical_address) VALUES (12, 0, 6, 'Log House in Palmyra');
+INSERT INTO public.address (keyid, client_id, employee_id, physical_address) VALUES (13, 0, 7, 'Near the palm, California');
+INSERT INTO public.address (keyid, client_id, employee_id, physical_address) VALUES (15, 0, 5, 'The House of the Music, WA');
+
 
 create table assignment
 (
@@ -92,6 +102,7 @@ INSERT INTO public.assignment (keyid, client_id, employee_id) VALUES (7, 6, 7);
 INSERT INTO public.assignment (keyid, client_id, employee_id) VALUES (8, 4, 1);
 INSERT INTO public.assignment (keyid, client_id, employee_id) VALUES (9, 2, 4);
 
+
 create table email
 (
     keyid         serial      not null
@@ -115,6 +126,15 @@ INSERT INTO public.email (keyid, client_id, employee_id, email_address) VALUES (
 INSERT INTO public.email (keyid, client_id, employee_id, email_address) VALUES (4, 0, 2, 'abraham.lincoln@usa.gov');
 INSERT INTO public.email (keyid, client_id, employee_id, email_address) VALUES (5, 0, 3, 'oprah@music.net');
 INSERT INTO public.email (keyid, client_id, employee_id, email_address) VALUES (6, 6, 0, 'sales@pricesmart.co');
+INSERT INTO public.email (keyid, client_id, employee_id, email_address) VALUES (7, 5, 0, 'sales@Addidas.com');
+INSERT INTO public.email (keyid, client_id, employee_id, email_address) VALUES (8, 0, 4, 'michael@jackson.com');
+INSERT INTO public.email (keyid, client_id, employee_id, email_address) VALUES (9, 0, 8, 'ashleyandpacho@mamail.com');
+INSERT INTO public.email (keyid, client_id, employee_id, email_address) VALUES (10, 1, 0, 'someone@epago.com');
+INSERT INTO public.email (keyid, client_id, employee_id, email_address) VALUES (11, 2, 0, 'deliciousdrink@coke.com');
+INSERT INTO public.email (keyid, client_id, employee_id, email_address) VALUES (12, 0, 6, 'prophet@restoredchurch.org');
+INSERT INTO public.email (keyid, client_id, employee_id, email_address) VALUES (13, 0, 7, 'greatfriend@buddies.com');
+INSERT INTO public.email (keyid, client_id, employee_id, email_address) VALUES (15, 0, 5, 'fancyhousie@taylorswift.com');
+
 
 create table phone
 (
@@ -139,3 +159,11 @@ INSERT INTO public.phone (keyid, client_id, employee_id, phone_number) VALUES (3
 INSERT INTO public.phone (keyid, client_id, employee_id, phone_number) VALUES (4, 0, 2, '+18014891545');
 INSERT INTO public.phone (keyid, client_id, employee_id, phone_number) VALUES (5, 0, 3, 'skype:oprahaccount');
 INSERT INTO public.phone (keyid, client_id, employee_id, phone_number) VALUES (6, 6, 0, '800-4567889');
+INSERT INTO public.phone (keyid, client_id, employee_id, phone_number) VALUES (7, 5, 0, '196145123541');
+INSERT INTO public.phone (keyid, client_id, employee_id, phone_number) VALUES (8, 0, 4, '198156123');
+INSERT INTO public.phone (keyid, client_id, employee_id, phone_number) VALUES (10, 0, 8, '275363636');
+INSERT INTO public.phone (keyid, client_id, employee_id, phone_number) VALUES (11, 1, 0, '4271854');
+INSERT INTO public.phone (keyid, client_id, employee_id, phone_number) VALUES (12, 2, 0, '18000000000');
+INSERT INTO public.phone (keyid, client_id, employee_id, phone_number) VALUES (13, 0, 6, '789456132');
+INSERT INTO public.phone (keyid, client_id, employee_id, phone_number) VALUES (14, 0, 7, '123456789');
+INSERT INTO public.phone (keyid, client_id, employee_id, phone_number) VALUES (17, 0, 5, 'facetime:taylorswift');
